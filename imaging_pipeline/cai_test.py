4ab9499223064d8c6abefeac57b6387db2750079
Martin Haesemeyer <m.haesemeyer@gmail.com>
2021-07-23 16:32:47 -0400

    Isolated analysis code from Experiment class by moving all analysis into new file analysis.py

--- imaging_pipeline/cai_test.py
+++ imaging_pipeline/cai_test.py
@@ -10,7 +10,7 @@ Script for testing caiman - cai_demo.py implementation with slight adjustments
 import numpy as np
 from utilities import ui_get_file
 import matplotlib.pyplot as pl
-from experiment import analyze_experiment
+import analysis as ana
 from os import path
 import logging
 import warnings
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     info_file = ui_get_file(filetypes=[('Experiment info', '*.info')], multiple=False)
     if type(info_file) == list:
         info_file = info_file[0]
-    exp = analyze_experiment(info_file, "OSU 2P", "", {"indicator_decay_time": 3.0})
+    exp = ana.analyze_experiment(info_file, "OSU 2P", "", {"indicator_decay_time": 3.0})
     acb_func = exp.avg_component_brightness(False)
 
     fig, axes = pl.subplots(ncols=int(np.sqrt(exp.n_planes))+1, nrows=int(np.sqrt(exp.n_planes)))
